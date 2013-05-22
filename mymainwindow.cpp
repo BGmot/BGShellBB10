c7c1d96617008fc38daf1841cfe3ad524cab3882
BGmot <support@tm-k.com>
2013-05-22 19:59:41 -0400

    Removing debugging lines.

--- mymainwindow.cpp
+++ mymainwindow.cpp
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2013 BGmot <support@tm-k.com>
  */
-#include <QtCore/QDebug>
+//#include <QtCore/QDebug>
 #include <QtCore/QEvent>
 #include "mymainwindow.h"
 #include "mysystemmenu.h"
@@ -15,7 +15,7 @@ CMyMainWindow::CMyMainWindow() :
 }
 
 bool CMyMainWindow::event(QEvent *e){
-	qDebug()<<"CMyMainWindow::event() type="<<e->type();
+//	qDebug()<<"CMyMainWindow::event() type="<<e->type();
 	if (e->type() ==  QEvent::PlatformPanel){
 		// Up-down swipe detected
 		if (SystemMenu->wdgSettingsWindow){
