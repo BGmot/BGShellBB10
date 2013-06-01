aebd2d2683ee8fb3683132ec6d934e170a9c04b8
BGmot <support@tm-k.com>
2013-06-01 13:10:53 -0400

    Payments temporarily removed because of windowing problem.

--- mysystemmenu.cpp
+++ mysystemmenu.cpp
@@ -25,6 +25,9 @@ int CMySystemMenu::MenuInit(){
 	btnDonate->setObjectName(QString::fromUtf8("btnDonate"));
 	btnDonate->setText(QString("Say Thanks"));
 
+	// For now disable donations
+	btnDonate->setVisible(false);
+
     QMetaObject::connectSlotsByName(this);
 
     SetGeometry();
@@ -35,6 +38,13 @@ void CMySystemMenu::SetGeometry(){
 	QRect r = QApplication::desktop()->screenGeometry(0);
 	int nScreenWidth = r.width();
 	this->setGeometry(0, 0, nScreenWidth, 103);
+	//this->setMinimumWidth(nScreenWidth);
+    QBrush brush1(QColor(76, 76, 76, 255));
+    brush1.setStyle(Qt::SolidPattern);
+    QPalette palette;
+    palette.setBrush(QPalette::Active, QPalette::Window, brush1);
+    this->setAutoFillBackground(true);
+
 	btnSettings->setGeometry(QRect(2, 1, nScreenWidth/2-2, 101));
 	btnDonate  ->setGeometry(QRect(nScreenWidth/2+2, 1, nScreenWidth/2-4, 101));
 }
